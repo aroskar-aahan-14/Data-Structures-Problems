85fe2f6a166ce39a86b1abdc0dc5df4c4a39e0f6
aroskar-aahan-14 <aahanaroskar211207@gmail.com>
2026-07-22 11:29:47 +0530

    Add files via upload

---
+++ practical_1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,x,count=0,b;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d",&n);
+    int a[50];
+    for(i=0;i<n;i++)
+    {
+        printf("Enter element number %d of the array: \n",i);
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the number x to search: ");
+    scanf("%d",&x);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==x)
+        {
+            count++;
+            if(count==1)
+            {
+                b=i+1;
+            }
+        }
+    }
+    if(count==0)
+    {
+        printf("The number searched does not exist in the array");
+    }
+    else if(count==1)
+    {
+        printf("The number searched %d is found in the array, but it is not repeating",x);
+    }
+    else
+    {
+        printf("The number searched %d is found in array and the number of occurrences of the searched number is %d\n",x,count);
+        printf("The position of the first occurrence of the searched number is: %d",b);
+    }
+    return 0;
+}
